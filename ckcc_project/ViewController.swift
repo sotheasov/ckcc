39235738b007b1b8e1b2b10be5bb87e3660be129
Sov Sothea <sovsothea@Sovs-MacBook-Pro.local>
2018-04-25 19:12:00 +0700

    init

---
+++ ckcc_project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ckcc_project
+//
+//  Created by Sov Sothea on 4/25/18.
+//  Copyright © 2018 Sov Sothea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
